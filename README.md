4db3a20c1715c02ad799988974275fe01b99b33a
Gleison Batista <gleisonbs@gmail.com>
2024-01-22 22:56:57 -0300

    initial files

---
+++ README.md
@@ -0,0 +1,1 @@
+# mandelbrot-python-v2
---
+++ main.py
@@ -0,0 +1,18 @@
+from PIL import Image
+
+from mandelbrot import MandelbrotSet
+
+mandelbrot_set = MandelbrotSet(max_iterations=20, escape_radius=1000)
+
+width, height = 512, 512
+scale = 0.0075
+GRAYSCALE = "L"
+
+image = Image.new(mode=GRAYSCALE, size=(width, height))
+for y in range(height):
+    for x in range(width):
+        c = scale * complex(x - width / 2, height / 2 - y)
+        instability = 1 - mandelbrot_set.stability(c, smooth=True)
+        image.putpixel((x, y), int(instability * 255))
+
+image.show()
